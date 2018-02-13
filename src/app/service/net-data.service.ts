85e2457795497db5b398b8c9ddfa68e9d2269859
1149023208@qq.com <zxxxxx11>
2018-02-14 00:02:14 +0800

    添加 新的服务 , 还没开始写方法

---
+++ src/app/service/net-data.service.ts
@@ -0,0 +1,86 @@
+import {Injectable} from '@angular/core';
+import {Todo} from './todo';
+
+@Injectable()
+export class NetDataService {
+
+  // Placeholder for last id so we can simulate
+  // automatic incrementing of id's
+  lastId: number = 0; // todo 改成跟后端的MongoDB 交互的
+
+  /**
+   * 数据放在这里  <br>
+   * Placeholder for to do's
+   * @type {Array}
+   */
+  todos: Todo[] = [];
+
+  constructor() {
+  }
+
+  // Simulate POST /todos
+  addTodo(todo: Todo): NetDataService {
+    if (!todo.id) {
+      todo.id = this.lastId + 1;
+    } else {
+      ;
+    }
+
+    this.todos.push(todo);
+    return this;
+  }
+
+  /**
+   *
+   * @param {Todo} todo
+   * @returns {number} 最新的id
+   */
+  addOne(todo: Todo): number {
+    if (!todo.id) {
+      todo.id = this.lastId + 1;
+    } else {
+      ;
+    }
+
+    this.todos.push(todo);
+    return this.lastId;
+  }
+
+  // Simulate DELETE /todos/:id
+  deleteTodoById(id: number): NetDataService {
+    this.todos = this.todos
+      .filter(todo => todo.id !== id);
+    return this;
+  }
+
+  // Simulate PUT /todos/:id
+  updateTodoById(id: number, values: Object = {}): Todo {
+    const todo = this.getTodoById(id);
+    if (!todo) {
+      return null;
+    }
+    Object.assign(todo, values);
+    return todo;
+  }
+
+  // Simulate GET /todos
+  getAllTodos(): Todo[] {
+    return this.todos;
+  }
+
+  // Simulate GET /todos/:id
+  getTodoById(id: number): Todo {
+    return this.todos
+      .filter(todo => todo.id === id)
+      .pop();
+  }
+
+  // Toggle to do complete
+  toggleTodoComplete(todo: Todo): Todo {
+    const updatedTodo = this.updateTodoById(todo.id, {
+      complete: !todo.complete
+    });
+    return updatedTodo;
+  }
+
+}
