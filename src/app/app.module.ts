554be63ac4a3e27bcf065f53afd3486cec5890bd
1149023208@qq.com <zxxxxx11>
2018-02-13 23:53:49 +0800

    添加提取出来的组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TodoInListComponent } from './todo-in-list/todo-in-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoInListComponent
   ],
   imports: [
     BrowserModule,
